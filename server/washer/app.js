a980d1704e68e9e1382d2480f19872dbdf0d08de
mumthaz <r4raheesamumthaz@gmail.com>
2021-09-10 17:41:52 +0530

    sagger aded for Washer

--- server/washer/app.js
+++ server/washer/app.js
@@ -1,42 +1,68 @@
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const profileRoutes= require('./routes/profileRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const myordersRoutes = require('./routes/myordersRoutes');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const myordersRoutes = require("./routes/myordersRoutes");
 
 //middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 //Connect to Customers Database
-const dbURI="mongodb+srv://raheesa:admin@cluster0.7m9bb.mongodb.net/Washer?retryWrites=true&w=majority"
+const dbURI =
+  "mongodb+srv://raheesa:admin@cluster0.7m9bb.mongodb.net/Washer?retryWrites=true&w=majority";
 mongoose
   .connect(dbURI, {
-    
     useNewUrlParser: true,
-    useUnifiedTopology: true
- 
+    useUnifiedTopology: true,
   })
-  .then(x => {
+  .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
   })
-  .catch(err => {
+  .catch((err) => {
     console.error("Error connecting to mongo", err);
   });
-app.use('/',[authRoutes,profileRoutes,orderRoutes,myordersRoutes]);
+app.use("/", [authRoutes, profileRoutes, orderRoutes, myordersRoutes]);
+
+//Swagger Config
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Washer API",
+      description: "API's for Washer -On demand carwash system",
+      server: ["http://localhost:4000"],
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT" 
+          }
+        }
+      }
+      
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); 
+
 app.listen(4000, () => console.log(`Listening on: 4000`));
 //module.exports.handler = serverless(app);
-
--- server/washer/routes/authRoutes.js
+++ server/washer/routes/authRoutes.js
@@ -6,12 +6,59 @@ const authController = require('../controllers/authController');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const router = Router();
 
-
-//Routes for signup
+/**
+ * @swagger
+ * /signup:
+ *  post:
+ *    tags: ['auth']
+ *    description: signup a new washer
+ *    parameters:
+ *      - in: body
+ *        name: Washer
+ *        description: The washer details for signup.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - email
+ *            - password
+ *          properties:
+ *            email:
+ *              type: string
+ *            password:
+ *              type: string         
+ *    responses:
+ *      '201':
+ *        description: A successful response
+ *      '400' :
+ *        description: Error occured
+ */
 router.post('/signup', urlencodedParser ,authController.post_signup);
-
-
-//Routes for login
+/**
+ * @swagger
+ * /login:
+ *  post:
+ *    tags: ['auth']
+ *    description: login a new washer
+ *    parameters:
+ *      - in: body
+ *        name: Washer
+ *        description: The washer details for login.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - email
+ *            - password
+ *          properties:
+ *            email:
+ *              type: string
+ *            password:
+ *              type: string         
+ *    responses:
+ *      '201':
+ *        description: JWT token of Washer
+ *      '400' :
+ *        description: Error occured
+ */
 router.post('/login', urlencodedParser ,authController.post_login);
 
 
